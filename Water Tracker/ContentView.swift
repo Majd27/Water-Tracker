f91a283aed0b566df81c4588db41496a47bd2bd6
Majd <llmajdll22@gmail.com>
2023-10-05 12:29:38 +0300

    Initial Commit

---
+++ Water Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Water Tracker
+//
+//  Created by Majd Abdullah on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Water Tracker/Water_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Water_TrackerApp.swift
+//  Water Tracker
+//
+//  Created by Majd Abdullah on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Water_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
